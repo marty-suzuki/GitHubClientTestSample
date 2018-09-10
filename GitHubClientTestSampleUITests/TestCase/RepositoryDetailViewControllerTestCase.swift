74584f1a1c85c97249e296882096113e3c56200a
marty-suzuki <s1180183@gmail.com>
2018-09-11 00:15:24 +0900

    page view checking

--- GitHubClientTestSampleUITests/TestCase/RepositoryDetailViewControllerTestCase.swift
+++ GitHubClientTestSampleUITests/TestCase/RepositoryDetailViewControllerTestCase.swift
@@ -16,83 +16,26 @@ final class RepositoryDetailViewControllerTestCase: XCTestCase {
     
     override func setUp() {
         super.setUp()
-
-        RepositoryDispatcher.shared.clearAll.accept(())
         helper.currentNavigationController.setViewControllers([], animated: false)
     }
 
     func testTrackingEvent_pageView_repositoryDetail() {
-        let pagination = helper.paginationMock()
         let repository = helper.repositoryMock()
-
-        RepositoryDispatcher.shared.repositoriesAndPagination.accept(([repository], pagination))
-
-        let vc = helper.showRepositorySearch()
-
-        helper.wait(timeout: 1)
-
-        let expect1: XCTestExpectation
-        let disposable: Disposable
-        do {
-            expect1 = expectation(description: "wait TrackingEvent.pageView(.repositoryDetail)")
-            disposable = TrackingDispatcher.shared.trackingContainer
-                .flatMap { trackingContainer -> Observable<URL> in
-                    if case let .pageView(.repositoryDetail(url)) = trackingContainer.event {
-                        return .just(url)
-                    }
-                    return .empty()
-                }
-                .subscribe(onNext: { url in
-                    XCTAssertEqual(url, repository.htmlURL)
-                    expect1.fulfill()
-                })
-        }
-
-        let expect2: XCTestExpectation
-        let observation: NSKeyValueObservation
-        do {
-            let detailVC: RepositoryDetailViewController = helper.waitViewController(timeout: 1) {
-                let tableView = vc.privateProperties.tableView!
-                let cell = tableView.visibleCells.first!
-                let rect = cell.convert(cell.bounds, to: vc.view)
-                vc.view.tap(at: CGPoint(x: rect.midX, y: rect.midY))
-            }
-
-            expect2 = expectation(description: "wait webView loading")
-            observation = detailVC.privateProperties.webView
-                .observe(\.isLoading, options: .new) { _, changed in
-                    if changed.newValue == false {
-                        expect2.fulfill()
-                    }
-                }
-        }
-
-        wait(for: [expect1, expect2], timeout: 10)
-        disposable.dispose()
-        observation.invalidate()
-    }
-
-    func testTrackingEvent_pageView_repositorySearch_when_back_button_item_tap() {
-        let repository = helper.repositoryMock()
-
-        let detailVC: RepositoryDetailViewController = helper.waitViewController(timeout: 1) {
-            RouteActionCreator.shared.setRouteCommand(.repositoryDetail(.object(repository)))
-        }
-
-        let expect = expectation(description: "wait TrackingEvent.pageView(.repositorySearch)")
+        
+        let expect = expectation(description: "wait TrackingEvent.pageView(.repositoryDetail)")
         let disposable = TrackingDispatcher.shared.trackingContainer
-            .flatMap { trackingContainer -> Observable<Void> in
-                if case .pageView(.repositorySearch) = trackingContainer.event {
-                    return .just(())
+            .flatMap { trackingContainer -> Observable<URL> in
+                if case let .pageView(.repositoryDetail(url)) = trackingContainer.event {
+                    return .just(url)
                 }
                 return .empty()
             }
-            .subscribe(onNext: {
+            .subscribe(onNext: { url in
+                XCTAssertEqual(url, repository.url)
                 expect.fulfill()
             })
 
-        // BackBarButtonItem tap
-        detailVC.view.tap(at: CGPoint(x: 20, y: 44))
+        RouteActionCreator.shared.setRouteCommand(.repositoryDetail(.object(repository)))
 
         wait(for: [expect], timeout: 5)
         disposable.dispose()
--- GitHubClientTestSampleUITests/TestCase/RepositorySearchViewControllerTestCase.swift
+++ GitHubClientTestSampleUITests/TestCase/RepositorySearchViewControllerTestCase.swift
@@ -16,7 +16,6 @@ final class RepositorySearchViewControllerTestCase: XCTestCase {
     override func setUp() {
         super.setUp()
 
-        RepositoryDispatcher.shared.clearAll.accept(())
         helper.currentNavigationController.setViewControllers([], animated: false)
     }
 
@@ -33,92 +32,9 @@ final class RepositorySearchViewControllerTestCase: XCTestCase {
                 expect.fulfill()
             })
 
-        helper.showRepositorySearch()
-
-        helper.wait(timeout: 1)
+        RouteActionCreator.shared.setRouteCommand(.repositorySearch)
 
         wait(for: [expect], timeout: 5)
         disposable.dispose()
-    }
-
-    func testTrackingEvent_search() {
-        let searchText = "swift"
-
-        let vc = helper.showRepositorySearch()
-        let searchBar = vc.privateProperties.searchBar
-
-        helper.wait(timeout: 1)
-
-        do {
-            let expect1 = expectation(description: "wait TrackingEvent.search")
-            let disposable1 = TrackingDispatcher.shared.trackingContainer
-                .flatMap { trackingContainer -> Observable<TrackingEvent.Search> in
-                    if case let .search(value) = trackingContainer.event {
-                        return .just(value)
-                    }
-                    return .empty()
-                }
-                .subscribe(onNext: { search in
-                    XCTAssertEqual(search.page, 1)
-                    XCTAssertEqual(search.query.lowercased(), searchText.lowercased())
-                    expect1.fulfill()
-                })
-
-            let expect2 = expectation(description: "wait search results")
-            let disposable2 = RepositoryStore.shared.repositories.asObservable()
-                .filter { !$0.isEmpty }
-                .subscribe(onNext: { _ in
-                    expect2.fulfill()
-                })
-
-            searchBar.becomeFirstResponder()
-            helper.wait(timeout: 1)
-
-            searchBar.helper.inputText(searchText)
-            helper.wait(timeout: 1)
-
-            searchBar.delegate?.searchBarSearchButtonClicked?(searchBar)
-
-            wait(for: [expect1, expect2], timeout: 10)
-            disposable1.dispose()
-            disposable2.dispose()
-        }
-
-        helper.wait(timeout: 2)
-
-        do {
-            let tableView = vc.privateProperties.tableView!
-
-            let expect1 = expectation(description: "wait TrackingEvent.search")
-            let disposable1 = TrackingDispatcher.shared.trackingContainer
-                .flatMap { trackingContainer -> Observable<TrackingEvent.Search> in
-                    if case let .search(value) = trackingContainer.event {
-                        return .just(value)
-                    }
-                    return .empty()
-                }
-                .subscribe(onNext: { search in
-                    XCTAssertEqual(search.page, 2)
-                    XCTAssertEqual(search.query.lowercased(), searchText.lowercased())
-                    expect1.fulfill()
-                })
-
-            let expect2 = expectation(description: "wait search results")
-            let disposable2 = RepositoryStore.shared.repositories.asObservable()
-                .skip(1)
-                .filter { !$0.isEmpty }
-                .subscribe(onNext: { _ in
-                    expect2.fulfill()
-                })
-
-            vc.view.drag(from: CGPoint(x: 0, y: tableView.frame.origin.y + 150),
-                         to: CGPoint(x: 0, y: tableView.frame.origin.y + 50))
-
-            wait(for: [expect1, expect2], timeout: 10)
-            disposable1.dispose()
-            disposable2.dispose()
-        }
-
-        helper.wait(timeout: 2)
     }
 }
